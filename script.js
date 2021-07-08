e21aec9645b9ba8891e5fc530f9024f05e22c3cc
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-07-08 20:03:13 -0300

    C1 - Adicionado Requisito 1 e demais de forma dinamica, acrescentando requisito 3

---
+++ script.js
@@ -0,0 +1,40 @@
+const creatTittle = document.createElement('h1');
+document.body.appendChild(creatTittle).innerText = 'Paleta de Cores';
+creatTittle.id = 'title'; 
+
+
+const colorPalette = document.createElement('section');
+colorPalette.id = 'color-palette'
+document.body.appendChild(colorPalette);
+
+const color = ['black', 'pink', 'blue', 'Yellow']
+for(let index = 0; index < color.length; index += 1){
+    const divColor = document.createElement('div');
+    divColor.className = 'color';
+    divColor.style.backgroundColor = color[index];
+    colorPalette.appendChild(divColor);
+}
+
+
+let boardPixel = document.createElement('section');
+document.body.appendChild(boardPixel)
+
+let table = document.createElement('table');
+boardPixel.appendChild(table);
+table.id = 'pixel-board';
+
+function framePixels(){
+    for(let index = 0; index < 5; index += 1) {
+        let tableLine = document.createElement('tr')
+        table.appendChild(tableLine);
+        for(let index2 = 0; index2 < 5; index2 += 1) {
+            let tableCelula = document.createElement('td');
+            tableCelula.classList.add('pixel')
+            tableLine.appendChild(tableCelula)
+        }
+    }
+} 
+framePixels()
+
+// let startWhite = document.getElementsByClassName('pixel');
+// window.onload.startWhite.style.backgroundColor = 'white';
